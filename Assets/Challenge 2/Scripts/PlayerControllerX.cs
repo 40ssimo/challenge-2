9a940c6dc6f158b8f3eafa066cf1649cc6b504a5
Muhammad Ichsan Pratama <ichsansanpratama@gmail.com>
2024-05-02 06:13:07 +0700

    solved bug

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public bool cooldown = false;
+    public float lastPressTime = 0f;
+    public float cooldownDuration = 5f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !cooldown)
+        {
+            cooldown = true;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastPressTime = Time.time;
+        }
+
+        if (cooldown && Time.time - lastPressTime >= cooldownDuration)
+        {
+            cooldown = false;
+        }
+    }
+}
